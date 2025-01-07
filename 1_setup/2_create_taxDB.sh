e74d73d85d495522c2b21bfc1f672a35fd5347a7
kimchafer <132273163+kimchafer@users.noreply.github.com>
2025-01-07 15:10:35 +0900

    Update 2_create_taxDB.sh

--- 1_setup/2_create_taxDB.sh
+++ 1_setup/2_create_taxDB.sh
@@ -4,28 +4,11 @@ cd /storage2/jihoonkim/eDNA/ncbi
 wget https://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 tar -xvzf taxdump.tar.gz
 
-# run gb_to_fasta.py
-
-mmseqs createdb /storage2/jihoonkim/eDNA/ncbi/cox1/cox1_sequences_with_taxid.fasta \
-/storage2/jihoonkim/eDNA/mmseqsDB/cox1_taxDB
-
-mmseqs createtaxdb /storage2/jihoonkim/eDNA/mmseqsDB/cox1_taxDB \
-/storage2/jihoonkim/eDNA/mmseqsDB/mmseqs_taxDB \
+mmseqs createtaxdb /storage2/jihoonkim/eDNA/mmseqsDB/cox1/cox1_refDB \
+/storage2/jihoonkim/eDNA/mmseqsDB/taxonomy/mmseqs_taxDB \
 --ncbi-tax-dump /storage2/jihoonkim/eDNA/ncbi/taxonomy
 
 
-
-
-
-
-
-
-
-
-
-mmseqs createtaxdb /storage2/jihoonkim/eDNA/ncbi/cox1/cox1_sequences_with_taxid.fasta \
-/storage2/jihoonkim/eDNA/mmseqsDB/mmseqs_taxDB \
---ncbi-tax-dump /storage2/jihoonkim/eDNA/ncbi/taxonomy
 
 
 
